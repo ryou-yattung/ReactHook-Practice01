a170cb2f2f4d7150c0bb24d4513b14ef78372404
ryou-yattung <92421273+ryou-yattung@users.noreply.github.com>
2022-02-12 22:54:16 +0900

    json

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-3xl text-center mt-4">Hello world!</h1>
+      const element = <FontAwesomeIcon icon={faCoffee} />
     </div>
   );
 }
