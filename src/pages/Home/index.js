96bc862510ba8d9e6cd263b6d712860acf26a7e1
ryou-yattung <92421273+ryou-yattung@users.noreply.github.com>
2022-03-02 20:20:12 +0900

    react

---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Edit from "./components/Edit";
+import List from "./components/List";
+
+const index = () => {
+  return (
+    <div>
+      aaa
+      <Edit />
+      <List />
+    </div>
+  );
+};
+
+export default index;
